6a15802f3b3ad1dd58dd7b3604bd4238bbb290ff
Lee-sun-ah <cosmos1526@dgu.edu>
2021-10-09 17:56:55 +0900

    초기페이지

--- main/views.py
+++ main/views.py
@@ -4,5 +4,8 @@ from .models import First
 # Create your views here.
 
 def index(request):
+    return render(request,'main/index.html')
+
+def first(request):
     datas=First.objects.all()
-    return render(request,'main/index.html',{"datas":datas})
+    return render(request,'main/firstpage.html',{"datas":datas})
